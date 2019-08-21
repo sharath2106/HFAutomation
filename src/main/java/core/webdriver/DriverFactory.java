c7443d4d3136a0387dbc61a2c4c97750fde3b7ce
sharath2106 <gopi.shanky@gmail.com>
2019-08-21 13:59:10 +0530

    :art: Updating DriverFactory class

---
+++ src/main/java/core/webdriver/DriverFactory.java
@@ -0,0 +1,32 @@
+package core.webdriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverFactory {
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--kiosk");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
+        driver.get("http://automationpractice.com/index.php");
+    }
+
+    public void tearDown() {
+        driver.quit();
+    }
+}
